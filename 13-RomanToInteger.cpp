41b9725fd3d5912a52dfe1546e9671f32e53d8ce
Sayed Abdo <Sayedabdo4900@gmail.com>
2023-03-07 16:23:14 +0200

    first problem

---
+++ 13-RomanToInteger.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+
+    int romanToInt(string s) {
+        unordered_map<char, int> ump = {{'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}};
+        int lastIndexAdded = -1 ;
+        int res = 0 ;
+        for (int i = 0 ; i < s.size() ; i++){
+            if (ump[s[i]] < ump[s[i+1]])
+                res-=ump[s[i]];
+            else {
+                res+=ump[s[i]] ;
+            }
+        }
+       // if (s[s.size()-2] < s[s.size()-1] )
+           // res+=ump[s[s.size()-1]];
+        return res;
+    }
+};
+int main() {
+    Solution s ;
+    cout << s.romanToInt("LVIII");
+    return 0;
+}
