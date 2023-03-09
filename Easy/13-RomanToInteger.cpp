95375909ba7d93e43fdf4d973b5f9d59b144e27f
Sayed Abdo <Sayedabdo4900@gmail.com>
2023-03-09 14:14:14 +0200

    modify the first problem

--- Easy/13-RomanToInteger.cpp
+++ Easy/13-RomanToInteger.cpp
@@ -8,9 +8,9 @@ public:
 
     int romanToInt(string s) {
         unordered_map<char, int> ump = {{'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}};
-     
+        s+=" ";
         int res = 0 ;
-        for (int i = 0 ; i < s.size() ; i++){
+        for (int i = 0 ; i < s.size()-1 ; i++){
             if (ump[s[i]] < ump[s[i+1]])
                 res-=ump[s[i]];
             else {
@@ -25,5 +25,6 @@ public:
 int main() {
     Solution s ;
     cout << s.romanToInt("LVIII");
+   
     return 0;
 }
